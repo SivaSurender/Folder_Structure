2384692d62217be200fa85b1dc089bcff81370af
SivaSurender <e.surender14@gmail.com>
2024-04-19 20:55:50 +0530

    state uniquness

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
 import { IoMdAdd } from "react-icons/io";
 
-function Folder() {
-  const [isAddVisible, setIsAddvisible] = useState(false);
+function Folder({ name, identifier }) {
+  const [isAddVisible, setIsAddvisible] = useState(null);
+  console.log(identifier, "dsdj");
   return (
     <div
       className="main_container__folder"
-      onMouseEnter={() => setIsAddvisible(true)}
-      onMouseLeave={() => setIsAddvisible(false)}
+      onMouseEnter={() => setIsAddvisible(identifier)}
+      onMouseLeave={() => setIsAddvisible(null)}
     >
-      {isAddVisible && (
+      {identifier === isAddVisible && (
         <IoMdAdd
           style={{ paddingTop: "4px", cursor: "pointer" }}
           onClick={() => {}}
         />
       )}
-      <div className="folder_container">Folder</div>
+      <div className="folder_container">{name}</div>
     </div>
   );
 }
--- src/components/Tree.jsx
+++ src/components/Tree.jsx
@@ -3,14 +3,16 @@ import Folder from "./Folder";
 import { useApp } from "../hooks/useApp";
 
 function Tree() {
-  const { mainState, setMainState } = useApp();
+  const { mainState } = useApp();
 
   console.log(mainState, "dfsdfj");
   return (
     <div className="main-tree__container">
-      <div className="main-sub__conatiner">
-        <Folder />
-      </div>
+      {mainState.map((item) => (
+        <div key={item.identifier} className="main-sub__conatiner">
+          <Folder {...item} />
+        </div>
+      ))}
     </div>
   );
 }
