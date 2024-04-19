4e7b598c532adadaaf31dfed4b262fb6d06907ef
SivaSurender <e.surender14@gmail.com>
2024-04-19 23:00:49 +0530

    name change functionality

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { IoMdAdd } from "react-icons/io";
 import { useApp } from "../hooks/useApp";
+import { SiTicktick } from "react-icons/si";
 
-function Folder({ name, identifier }) {
+function Folder({ name, identifier, isEditOpen }) {
   const [isAddVisible, setIsAddvisible] = useState(null);
+  const [addedName, setAddedName] = useState(name);
 
   const { setMainState } = useApp();
 
@@ -25,25 +27,56 @@ function Folder({ name, identifier }) {
             type: "folder",
             name: "foldertwo",
             hasChild: false,
+            isEditOpen: true,
           },
         ],
       };
     });
   };
 
+  const hanldleNameSave = (id) => {
+    setMainState((prev) => {
+      console.log({ id, prev }, "dfsjfh");
+      return {
+        ...prev,
+        data: prev.data.map((item) =>
+          item.identifier === id
+            ? { ...item, name: addedName, isEditOpen: false }
+            : item
+        ),
+      };
+    });
+  };
   return (
     <div
       className="main_container__folder"
       onMouseEnter={() => setIsAddvisible(identifier)}
       onMouseLeave={() => setIsAddvisible(null)}
     >
-      {identifier === isAddVisible && (
+      {identifier === isAddVisible && isEditOpen && (
         <IoMdAdd
           style={{ paddingTop: "4px", cursor: "pointer" }}
           onClick={() => addFolderHandle(identifier)}
         />
       )}
-      <div className="folder_container">{name}</div>
+      <div className="folder_container">
+        {isEditOpen ? (
+          <>
+            <input
+              defaultValue={name}
+              id={identifier}
+              className="folder_input"
+              onChange={(e) => setAddedName(e.target.value)}
+            />
+            <SiTicktick
+              style={{ cursor: "pointer" }}
+              onClick={() => hanldleNameSave(identifier)}
+            />
+          </>
+        ) : (
+          name
+        )}
+      </div>
     </div>
   );
 }
