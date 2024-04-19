2964b2905664da7b6eff124a92b2ddcc0f397e57
SivaSurender <e.surender14@gmail.com>
2024-04-19 20:27:26 +0530

    folder struct, icon package

---
+++ src/components/Folder.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Folder() {
+  return <div className="folder_container">Folder</div>;
+}
+
+export default Folder;
---
+++ src/components/Tree.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Folder from "./Folder";
+
+function Tree() {
+  return (
+    <div className="main-tree__container">
+      <div className="main-sub__conatiner">
+        <Folder />
+      </div>
+    </div>
+  );
+}
+
+export default Tree;
